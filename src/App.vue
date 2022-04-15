d5cd1b27e576f62d7b472ffbc53a4e6379746ee9
moon250 <mooneupho@gmail.com>
2022-04-15 19:41:50 +0200

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <nav class="links">
+      <ul>
+        <li>About me</li>
+        <li>Skills</li>
+        <li>Projects</li>
+      </ul>
+      <svg class="burger-menu">
+        <use xlink:href="sprite.svg#menu"></use>
+      </svg>
+    </nav>
+  </header>
+  <main>
+    <svg class="logo">
+      <use xlink:href="sprite.svg#logo"></use>
+    </svg>
+    <div class="socials">
+      <svg class="icon">
+        <use xlink:href="sprite.svg#discord"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="sprite.svg#instagram"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="sprite.svg#github"></use>
+      </svg>
+    </div>
+  </main>
+  <footer></footer>
+</template>
