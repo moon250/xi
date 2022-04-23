34ce3cf71d1f5ff8375445176e498c1c11a9aad4
moon250 <mooneupho@gmail.com>
2022-04-23 22:43:52 +0200

    feat: footer

--- src/components/Parts/Footer.vue
+++ src/components/Parts/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <div>
-      <p>Made with ❤️ by moon250</p>
+      <p>Made with❤️by moon250</p>
       <Socials/>
     </div>
     <p>&copy; moon250, 2022 - All rights reserved</p>
