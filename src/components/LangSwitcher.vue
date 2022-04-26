88f6a9050714f89c707cdf8f39493d4f644019bd
moon250 <mooneupho@gmail.com>
2022-04-26 23:54:51 +0200

    feat: lang switcher component

--- src/components/LangSwitcher.vue
+++ src/components/LangSwitcher.vue
@@ -1,6 +1,18 @@
 <template>
-
+  <Icon class="lang-switcher" :name="`flag_${langs[currentLang]}`" :icon-class="false" @click="switchLanguage"/>
 </template>
 
 <script setup>
+import Icon from "./Icon.vue";
+import { ref } from "vue";
+
+const currentLang = ref(0);
+const langs = [
+  "en",
+  "fr"
+];
+
+function switchLanguage () {
+  currentLang.value = currentLang.value + 1 < langs.length ? currentLang.value + 1 : currentLang.value - 1;
+}
 </script>
