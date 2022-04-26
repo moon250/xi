3745e0f830f3fca96bf36bd79b465c3c2121275b
moon250 <mooneupho@gmail.com>
2022-04-27 00:19:20 +0200

    feat: add working theme switch

--- src/App.vue
+++ src/App.vue
@@ -13,4 +13,19 @@ import AboutMe from "./components/Parts/AboutMe.vue";
 import Skills from "./components/Parts/Skills.vue";
 import Projects from "./components/Parts/Projects.vue";
 import Logo from "./components/Parts/Logo.vue";
+
+let currentMode = parseInt(localStorage.getItem("theme"));
+const root = document.documentElement;
+
+if (currentMode === 0) {
+  root.style.setProperty("--primary", "var(--light)");
+  root.style.setProperty("--rgb-primary", "var(--rgb-light)");
+  root.style.setProperty("--secondary", "var(--dark)");
+  root.style.setProperty("--rgb-secondary", "var(--rgb-dark)");
+} else {
+  root.style.setProperty("--primary", "var(--dark)");
+  root.style.setProperty("--rgb-primary", "var(--rgb-dark)");
+  root.style.setProperty("--secondary", "var(--light)");
+  root.style.setProperty("--rgb-secondary", "var(--rgb-light)");
+}
 </script>
--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="theme-switcher__container">
-    <input class="theme-switcher__checkbox" type="checkbox" name="" id="">
+    <input class="theme-switcher__checkbox" type="checkbox" @click="switchTheme">
     <Icon name="sun" class="theme-switcher__icon" id="sun"/>
     <Icon name="moon" class="theme-switcher__icon" id="moon"/>
   </div>
@@ -8,4 +8,31 @@
 
 <script setup>
 import Icon from "./Icon.vue";
+
+if (!localStorage.hasOwnProperty("theme")) localStorage.setItem("theme", "0");
+
+const root = document.documentElement;
+const themes = [
+  "light",
+  "dark"
+];
+
+function switchTheme () {
+  let currentMode = parseInt(localStorage.getItem("theme"));
+  currentMode = currentMode + 1 < themes.length ? currentMode + 1 : currentMode - 1;
+  localStorage.setItem("theme", currentMode.toString());
+
+  if (currentMode === 0) {
+    root.style.setProperty("--primary", "var(--light)");
+    root.style.setProperty("--rgb-primary", "var(--rgb-light)");
+    root.style.setProperty("--secondary", "var(--dark)");
+    root.style.setProperty("--rgb-secondary", "var(--rgb-dark)");
+    return;
+  }
+
+  root.style.setProperty("--primary", "var(--dark)");
+  root.style.setProperty("--rgb-primary", "var(--rgb-dark)");
+  root.style.setProperty("--secondary", "var(--light)");
+  root.style.setProperty("--rgb-secondary", "var(--rgb-light)");
+}
 </script>
