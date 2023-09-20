97a81df007abda85d6295a4be6758045ee1b5093
moon250 <mooneupho@gmail.com>
2023-09-20 21:36:45 +0200

    ref: make projects' corners yellow

--- src/components/Parts/Projects.vue
+++ src/components/Parts/Projects.vue
@@ -7,7 +7,29 @@
   >
     <h1 id="projects__title">{{ $t("titles.projects") }}</h1>
     <div class="project__list">
-      <div class="project__item" v-for="project in projects">
+      <div
+        class="project__item"
+        v-for="project in projects"
+        :key="project.name"
+        @mouseover="hover"
+      >
+        <svg
+          class="project__corner"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 34 34"
+          fill="none"
+        >
+          <path d="M1 1H15.9677V18.0323H33V33H1V1Z" />
+        </svg>
+        <svg
+          class="project__corner"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 34 34"
+          fill="none"
+        >
+          <path d="M1 1H15.9677V18.0323H33V33H1V1Z" />
+        </svg>
+
         <img :src="'/img/' + project.thumbnail" :alt="project.name" />
         <div class="project__description">
           <h2>{{ project.name }}</h2>
@@ -22,4 +44,8 @@
 
 <script setup>
 import { projects } from "../../content/projects.json";
+import { ref } from "vue";
+
+const hovered = ref(false);
+const hover = () => (hovered.value = !hovered.value);
 </script>
