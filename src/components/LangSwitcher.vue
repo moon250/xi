26ba36031496b87a691942aabf9ee29aaf92c742
moon250 <mooneupho@gmail.com>
2023-09-20 21:18:48 +0200

    fix: spacings and margins

--- src/components/LangSwitcher.vue
+++ src/components/LangSwitcher.vue
@@ -23,6 +23,5 @@ function switchLanguage() {
   currentLang.value = currentLang.value === "fr" ? "en" : "fr";
   localStorage.setItem("lang", currentLang.value);
   i18n.locale.value = currentLang.value;
-  console.log(currentLang.value);
 }
 </script>
